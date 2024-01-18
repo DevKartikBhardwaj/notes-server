eff4bd30b13efdc53428c60c03aab585b272fafc
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2024-01-18 13:11:36 +0530

    dbug-6

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -133,6 +133,8 @@ router.post("/login", async (req, res) => {
         res.cookie("auth_token", token, {
           expires: new Date(Date.now() + 9000000000),
           sameSite: "none",
+          secure: true,
+          httpOnly: false,
         });
         res
           .status(200)
